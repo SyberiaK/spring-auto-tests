51bf27c843c491decc899370656454635aa32743
SyberiaK <syberiakey@gmail.com>
2022-06-29 16:19:18 +0500

    Improved readability of code + new tests for order elements

--- test_order_page.py
+++ test_order_page.py
@@ -4,7 +4,7 @@ import time
 from pages.auth_page import AuthPage
 from pages.order_page import OrderPage
 
-page = ...
+page = OrderPage(..., ...)
 
 
 @pytest.fixture(scope='function', autouse=True)
@@ -14,43 +14,54 @@ def setup(browser):
     login_link = 'http://u1609007.isp.regruhosting.ru/#/auth'
     page = AuthPage(browser, login_link)
     page.open()
+
     page.should_be_auth_page()
     email = os.getenv('SPRING_ADMIN_MAIL')
     password = os.getenv('SPRING_ADMIN_PASSWORD')
     page.auth(email, password)
     page.should_not_be_auth_page()
+
     link = 'http://u1609007.isp.regruhosting.ru/#'
     page = OrderPage(browser, link)
     page.open()
 
 
-def test_can_see_order_create_button(browser):
-    page.should_be_create_order_button()
+class TestOrderBasis:
+    def test_can_see_order_create_button(self, browser):
+        page.should_be_create_order_button()
+
+    def test_can_see_order_creation(self, browser):
+        page.should_be_create_order_button()
+        page.go_to_create_order()
+        page.should_be_order_creation()
+
+    def test_cant_see_order_creation(self, browser):
+        page.should_be_create_order_button()
+        page.should_not_be_order_creation()
+
+    def test_can_cancel_order_creation(self, browser):
+        page.go_to_create_order()
+        page.go_to_cancel_order()
+        page.cancel('leave')
+        page.should_not_be_order_creation()
+
+    def test_can_go_back_to_order_creation(self, browser):
+        page.go_to_create_order()
+        page.go_to_cancel_order()
+        page.cancel('cancel')
+        page.should_be_order_creation()
 
 
-def test_can_see_order_creation(browser):
-    page.should_be_create_order_button()
-    page.click_create_order()
-    page.should_be_order_creation()
-
-
-def test_cant_see_order_creation(browser):
-    page.should_be_create_order_button()
-    page.should_not_be_order_creation()
-
-
-class TestCreationOrder:
+class TestOrderCreation:
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
         page.should_be_create_order_button()
         self.order_number = f'AUTOTEST_{int(time.time())}'
         page.create_order(self.order_number)
 
-    def test_can_create_order(self, browser):
-        pass
-
     def test_can_see_order_draft_after_creation(self, browser):
         page.should_be_order_draft()
+        page.should_not_be_order_creation()
 
     def test_expected_order_info_equals_actual(self, browser):
         page.open()
@@ -61,12 +72,14 @@ class TestCreationOrder:
 
     def test_edit_and_save_see_summary(self, browser):
         order_number = f'EDITED_{self.order_number}'
-        page.edit_order_field('number', order_number)
         date = '30.03.2002'
-        page.edit_order_field('date', date)
         provider = '123123'
+
+        page.edit_order_field('number', order_number)
+        page.edit_order_field('date', date)
         page.edit_order_field('provider', provider)
         page.save_order()
+
         link = 'http://u1609007.isp.regruhosting.ru/#'
         same_page = OrderPage(browser, link)
         same_page.open()
@@ -74,45 +87,200 @@ class TestCreationOrder:
 
     def test_edit_and_save_see_draft(self, browser):
         order_number = f'EDITED_{self.order_number}'
-        page.edit_order_field('number', order_number)
         date = '30.03.2002'
-        page.edit_order_field('date', date)
         shop = 'Shop 1 Shop 1 Shop 1 Shop 1 Shop 1 Shop 1 Shop 1 Shop 1 Shop 1 Shop 1 Shop 1 Shop 1 Shop 1 Shop 1 Shop 1'
-        page.edit_order_field('shop', shop)
         provider = '123123'
+
+        page.edit_order_field('number', order_number)
+        page.edit_order_field('date', date)
+        page.edit_order_field('shop', shop)
         page.edit_order_field('provider', provider)
         page.save_order()
+
         link = 'http://u1609007.isp.regruhosting.ru/#'
         same_page = OrderPage(browser, link)
         same_page.open()
         same_page.go_to_order()
         same_page.check_order_draft_info(order_number, date, shop, provider)
 
-    def test_edit_see_summary(self, browser):
+    def test_edit_and_not_save_see_summary(self, browser):
         order_number = f'EDITED_{self.order_number}'
-        page.edit_order_field('number', order_number)
         date = '30.03.2002'
-        page.edit_order_field('date', date)
         provider = '123123'
+
+        page.edit_order_field('number', order_number)
+        page.edit_order_field('date', date)
         page.edit_order_field('provider', provider)
         # page.save_order()    # не сохраняем!
+
         link = 'http://u1609007.isp.regruhosting.ru/#'
         same_page = OrderPage(browser, link)
         same_page.open()
         same_page.check_order_info(self.order_number)    # поэтому сверяем со старыми данными (дефолтными)
 
-    def test_edit_see_summary(self, browser):
+    def test_edit_and_not_save_see_draft(self, browser):
         order_number = f'EDITED_{self.order_number}'
-        page.edit_order_field('number', order_number)
         date = '30.03.2002'
-        page.edit_order_field('date', date)
         shop = 'Shop 1 Shop 1 Shop 1 Shop 1 Shop 1 Shop 1 Shop 1 Shop 1 Shop 1 Shop 1 Shop 1 Shop 1 Shop 1 Shop 1 Shop 1'
-        page.edit_order_field('shop', shop)
         provider = '123123'
+
+        page.edit_order_field('number', order_number)
+        page.edit_order_field('date', date)
+        page.edit_order_field('shop', shop)
         page.edit_order_field('provider', provider)
         # page.save_order()    # не сохраняем!
+
         link = 'http://u1609007.isp.regruhosting.ru/#'
         same_page = OrderPage(browser, link)
         same_page.open()
         same_page.go_to_order()
         same_page.check_order_draft_info(self.order_number)    # поэтому сверяем со старыми данными (дефолтными)
+
+
+class TestElementBasis:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        page.should_be_create_order_button()
+        self.order_number = f'AUTOTEST_{int(time.time())}'
+        page.create_order(self.order_number)
+
+    def test_can_add_element(self, browser):
+        page.should_be_order_draft()
+
+    def test_can_see_element_creation(self, browser):
+        page.should_be_order_draft()
+        page.go_to_create_element()
+        page.should_be_element_creation()
+
+    def test_cant_see_element_creation(self, browser):
+        page.should_be_order_draft()
+        page.should_not_be_element_creation()
+
+    def test_can_cancel_element_creation(self, browser):
+        page.go_to_create_element()
+        page.go_to_cancel_element()
+        page.cancel('leave')
+        page.should_not_be_element_creation()
+
+    def test_can_go_back_to_order_creation(self, browser):
+        page.go_to_create_element()
+        page.go_to_cancel_element()
+        page.cancel('cancel')
+        page.should_be_element_creation()
+
+
+class TestElementCreation:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        page.should_be_create_order_button()
+        self.order_number = f'AUTOTEST_{int(time.time())}'
+        page.create_order(self.order_number)
+        page.should_be_order_draft()
+        page.create_element()
+
+    def test_can_see_order_draft_after_creation(self, browser):
+        page.should_be_order_draft()
+        page.should_not_be_element_creation()
+
+    def test_expected_element_info_equals_actual(self, browser):
+        page.check_element_info(self.order_number)
+
+    def test_save_and_reload(self, browser):
+        page.save_order()
+        link = 'http://u1609007.isp.regruhosting.ru/#'
+        same_page = OrderPage(browser, link)
+        same_page.open()
+        same_page.go_to_order()
+        page.should_be_any_element()
+
+    def test_not_save_and_reload(self, browser):
+        # page.save_order()  # не сохраняем!
+        link = 'http://u1609007.isp.regruhosting.ru/#'
+        same_page = OrderPage(browser, link)
+        same_page.open()
+        same_page.go_to_order()
+        page.should_not_be_any_element()  # поэтому элементов не должно быть
+
+    def test_delete_save_and_reload(self, browser):
+        page.go_to_delete_element()
+        page.delete_element('delete')
+        page.save_order()
+        link = 'http://u1609007.isp.regruhosting.ru/#'
+        same_page = OrderPage(browser, link)
+        same_page.open()
+        same_page.go_to_order()
+        page.should_not_be_any_element()
+
+    def test_delete_save_and_reload(self, browser):
+        page.go_to_delete_element()
+        page.delete_element('delete')
+        # page.save_order()  # не сохраняем!
+        link = 'http://u1609007.isp.regruhosting.ru/#'
+        same_page = OrderPage(browser, link)
+        same_page.open()
+        same_page.go_to_order()
+        page.should_not_be_any_element()  # поэтому элемент должен остаться
+
+    def test_can_cancel_deletion(self, browser):
+        page.go_to_delete_element()
+        page.delete_element('cancel')
+        page.should_be_any_element()
+
+    def test_can_add_few_elements(self, browser):
+        page.create_element()
+        page.create_element()
+        page.count_elements(3)
+
+    def test_can_add_few_elements_and_save(self, browser):
+        page.create_element()
+        page.create_element()
+        page.count_elements(3)
+        page.save_order()
+        link = 'http://u1609007.isp.regruhosting.ru/#'
+        same_page = OrderPage(browser, link)
+        same_page.open()
+        same_page.go_to_order()
+        page.count_elements(3)
+
+    def test_can_add_few_elements_and_not_save(self, browser):
+        page.create_element()
+        page.create_element()
+        page.count_elements(3)
+        # page.save_order()  # не сохраняем!
+        link = 'http://u1609007.isp.regruhosting.ru/#'
+        same_page = OrderPage(browser, link)
+        same_page.open()
+        same_page.go_to_order()
+        page.should_not_be_any_element()  # поэтому элементов не должно быть
+
+    def test_can_add_few_elements_and_save_then_add_few_elements_and_save(self, browser):
+        page.create_element()
+        page.create_element()
+        page.count_elements(3)
+        page.save_order()
+        page.create_element()
+        page.create_element()
+        page.create_element()
+        page.count_elements(6)
+        page.save_order()
+        link = 'http://u1609007.isp.regruhosting.ru/#'
+        same_page = OrderPage(browser, link)
+        same_page.open()
+        same_page.go_to_order()
+        page.count_elements(6)
+
+    def test_can_add_few_elements_and_save_then_add_few_elements_and_not_save(self, browser):
+        page.create_element()
+        page.create_element()
+        page.count_elements(3)
+        page.save_order()
+        page.create_element()
+        page.create_element()
+        page.create_element()
+        page.count_elements(6)
+        # page.save_order()  # не сохраняем!
+        link = 'http://u1609007.isp.regruhosting.ru/#'
+        same_page = OrderPage(browser, link)
+        same_page.open()
+        same_page.go_to_order()
+        page.count_elements(3)  # поэтому элементов должно быть 3
